4c31e284662140af1380ffc215064f1e7850627d
smai000 <smai000@hotmail.com>
2015-03-28 20:29:41 -0700

    test

---
+++ ContactUs.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Universal Metal Supply Ltd. Contact Us</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Specializes in providing a wide variety of high quality aluminum extrusion,fully welded maintenance free aluminum railing, fence, privacy panel, gate, spiral stairs, Supply and Installation in Calgary">
+<meta name="keywords" content="aluminum extrusions Calgary, aluminum railing Calgary, aluminum picket railing Calgary,aluminum glass railing Calgary, spiral stair Calgary, privacy panel, aluminum fence Calgary, aluminum gates Calgary, wind wall Calgary">
+
+<script type="text/javascript">
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+</script>
+<style type="text/css">
+a:link {
+	color: #FFF;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #FFF;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+	font-family: Verdana, Geneva, sans-serif;
+}
+</style>
+</head>
+
+<body onload="MM_preloadImages('Image/Home/WEBSITE---HOME---PIC-ONE.jpg','Image/Home/WEBSITE---HOME---PIC-TWO.jpg','Image/Home/WEBSITE---HOME---PIC-THREE.jpg','Image/Home/WEBSITE---HOME---PIC-FOUR.jpg','Image/Home/WEBSITE---HOME---PIC-FIVE.jpg')">
+
+<div class="Frame"; style="width:auto; height:auto; background-image:url(Image/Home/WEBSITE---HOME---BACKGROUND.jpg); background-position: left; background-repeat:repeat;">
+
+<div class="Header"; style="margin-top:22px;margin: auto; width:810px; height:158px; background-image:url(Image/Home/WEBSITE---HOME---HEADER.JPG)">
+
+<div style="width:810px; height: 110px;">
+<div style="float:left; width:350px;height:110px;"> 
+  <img src="Image/Home/LOGO.png" width="350" height="108" /></div>
+  <!----UMS---->
+  
+<div style="margin-right:10px; float: right; width:346px; height:110px; background-image:url(Image/Home/WEBSITE---HOME---RIGHT-CORNER.jpg)"><!------------Facebook---------->
+</div>
+<!------------LOGO-------------->
+</div>
+<!-----------UMS & FB----------->
+
+<div style="margin-top:3px; height: 40px; font-size: 12px; color: #FFF; text-decoration: blink; font-family: Verdana, Geneva, sans-serif;">
+  <table width="730" height="49" border="0">
+    <tr>
+      <td width="100" height="45"><div style="margin-left:12px;">
+        <div align="center"><strong><a href="index.html">HOME</a></strong></div>
+      </div></td>
+      <td width="100">&nbsp;</td>
+      <td width="100"><div align="center"><strong><a href="AboutUs.html">ABOUT US</a></strong></div></td>
+      <td width="100">&nbsp;</td>
+      <td width="100"><div align="center"><strong><a href="Products.html">PRODUCTS</a></strong></div></td>
+      <td width="100">&nbsp;</td>
+    <td width="100"><div align="center"><strong><a href="ContactUs.php">CONTACT US</a></strong></div></td>
+    </tr>
+  </table>
+</div>
+<!-----------Manu--------------->
+</div>
+<!----------Header End---------->
+
+<div class="body" style="width:810px; height:auto; margin:auto;background-image:url(Image/Home/WEBSITE---HOME---WHITE-BACKGROUND.jpg);">
+
+<div style ="margin-top: 8px; margin-right:10px; margin-left: 10px; width: 770px; height: auto color: #233198; font-size: 20px; font-style: arial;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10039.614389127839!2d-114.01925970077059!3d51.01792979614302!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53717a83aa86d3e5%3A0xecae67ce817315fb!2s4142+16+St+SE!5e0!3m2!1sen!2sca!4v1396508333534" width="780" height="400" frameborder="0" style="border:0"></iframe> <br /><small><a href="https://www.google.ca/maps/place/4142+16+St+SE,+Calgary,+AB+T2G+3S1/@51.0146968,-114.0179079,14z/data=!4m2!3m1!1s0x53717a83aa86d3e5:0xecae67ce817315fb?hl=en" style="color:#0000FF;text-align:left">View Larger Map</a></small><br />
+</div>
+<!--------qwqerqr---------------->
+
+<div style ="width: 770px; height: auto; ">
+<div style="width:810px; height:520px;"><div style="margin-top: 10px; height: auto; width: 300px; float:left;">
+<div style="margin-top:3px; margin-bottom: 10px; width:247px; height:60px; background-image:url(Image/Home/WEBSITE---OUT-CONTACTUS.jpg)">
+</div>
+
+<div style="width:265px; height:90px; margin-top: 20px; font-size: 16px;">Contact Information
+  <div style=" float:left; width:42px; height:30px; margin-top:"><img src="Image/Home/envlop.png" /></div>
+  <div style="float:left; width:auto; height:80px; margin-left:5px; line-height:25px; font-size: 14px;">Address: 
+<br/>4142 16th Street SE, Calgary
+<br/>Alberta, Canada, T2G 3S1
+  </div></div>
+
+<div style="width: 265px; height:auto; margin-top: 20px;">
+  <div style=" float:left; width:42px; height:30px;"><img src="Image/Home/info.png" /></div>
+  <div style="float:left; width:auto; height:auto; margin-left:0px; line-height:25px; font-size: 14px;">E-mail : <a href="mailto:info@ums-cal.com" style=" color:#0099ca;">info@ums-cal.com</a>
+<br/>
+Ph : +1 (403) 681-0683
+</div>
+</div>
+
+<div style="width:auto; height:auto; margin-top: 10px; margin-left: 10px;"><img src="Image/Home/Warehouse.JPG" width="250" height="250" onclick="MM_openBrWindow('Image/Home/Warehouse.JPG','','width=3300,height=2500')" /></div>
+
+</div>
+<!------------Contact Us-------------->
+
+<div style="margin-top: 10px; margin-right:10px; margin-left: 15px; float: left; width: 450px; height: auto;">
+  <?php 
+
+if(isset($_REQUEST['email']))
+{ $email = $_REQUEST['email'] ;
+  $subject = $_REQUEST['name'] ;
+  $txt = $_REQUEST['textarea'] ;
+  $message="customer phone number:	".$_REQUEST['phone']."\n"."customer name:	".$_REQUEST['name']."\n"."message content:\n".$txt;
+  mail("universalmetalca@gmail.com", $subject,$message, "From:" . $email);
+  echo "<form method='post' action = 'ContactUs.php'> <br/><br/><font size='3' color='red'> Thank you for using our mail form </font>
+  <p><input type='submit' value='Back' /></p></form>";
+}
+else
+{echo "
+ <div class='c_form';>
+              <p style=' font-style: arial; color:#233198 ; font-size: 16px; margin:10px 0 0 0; padding:0;'><strong>Contact Form</strong></p>
+              <form method='post' action='ContactUs.php'>
+              <p>Your Name: <input type='text'  name='name' size='20'  /> Phone#:
+<input type='text'  name='phone' size='15' /></p>
+              <p>Your Email: <input type='text'  name='email' size='40' /></p>
+              Your Message: <br/><textarea name='textarea' cols='50' rows='10' >
+            
+              </textarea>
+             <p> <input type='submit'/>
+  </p></form>
+               
+            </div></p>
+            </div>";}
+?>
+</div>
+
+<div style="font-size:16px; margin-left:50px; "><strong>Thank you very much for your information, and we will get back to you as soon as possible.</strong></div>
+</div>
+
+<div style="height: 40px; width:808px;font-size: 10px; color: #FFF; text-decoration: blink;background-image:url(Image/Home/WEBSITE---HOME---bottom.jpg)">
+<div style="height:30px;"><p style="margin-left: 20px; margin-bottom: 10px; font-size: 10px; font-family: Verdana, Geneva, sans-serif;">
+Universal Metal Supply Ltd.<br/>
+Copyright@2014 ums-cal.com</p>	
+</div>
+</div>
+<!------footer------------------->
+</div>
+</div>
+</body>
+</html>
